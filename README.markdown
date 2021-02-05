89b53c474e961e71145648b5c46173a17244af10
Cole Crum <68294806+colecrum@users.noreply.github.com>
2021-02-05 11:44:50 -0600

    commit1

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Service Page
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/colecrum/pen/abBzVYo](https://codepen.io/colecrum/pen/abBzVYo).
+
+
---
+++ script.js
@@ -0,0 +1,26 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function responsiveNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+/* Collapsible FAQ sections */
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
